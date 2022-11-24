bce4651b2a2dd9debe840e6ec127c6679aa6644e
EdgarPinto38 <edgarpinto010203@gmail.com>
2022-11-24 10:27:28 -0600

    Agregando inserts

---
+++ bd/queries.sql
@@ -0,0 +1,6 @@
+INSERT INTO categorias (nombre, pesoLimiteSuperior,pesoLimiteInferior) VALUES
+("peso superpaja femenil",53,48),
+("peso paja femenil",52,47),
+("peso paja femenil",52,47),
+("peso paja femenil",52,47),
+("peso paja femenil",52,47);
